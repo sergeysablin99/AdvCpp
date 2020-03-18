1033f9d4706984e6c0fe0f481196d2e670aa3c8c
Sergey Sablin <serewkasablin@outlook.com>
2020-03-18 21:13:52 +0300

    .gitignore edits

---
+++ YaContest6/cmake-build-debug/CMakeFiles/YaContest6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/YaContest6.dir/main.cpp.o"
+  "YaContest6"
+  "YaContest6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/YaContest6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ YaContest6/cmake-build-debug/CMakeFiles/YaContest6.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for YaContest6.
+# This may be replaced when dependencies are built.
