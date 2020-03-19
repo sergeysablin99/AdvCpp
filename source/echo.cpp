a175ad34f259c5a8f0558d80a756b5d439a68416
sergeysablin99 <bess2010@inbox.ru>
2020-03-19 19:14:48 +0300

    Homework 1 files

---
+++ source/echo.cpp
@@ -0,0 +1,17 @@
+//
+// Created by gtl on 17.03.2020.
+//
+
+#include <unistd.h>
+#include <cstdio>
+
+int main() {
+    char buffer[5];
+    while (!feof(stdin))
+    {
+        size_t bytes = fread(buffer, 1, sizeof(buffer), stdin);
+        fflush(stdout);
+        fwrite(buffer, 1, bytes, stdout);
+    }
+    return 0;
+}
---
+++ source/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/process.hpp"
+
+int main() {
+    Process process("./echo");
+    return 0;
+}
